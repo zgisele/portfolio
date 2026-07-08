4bd6f06533e8281fea8f470aae1c1f1621cc5cb9
zgisele <kafouigisele@gmail.com>
2026-07-08 07:40:57 +0000

    gestion des options telecharcher cv et menu hamberger

--- css/js/app.js
+++ css/js/app.js
@@ -1,33 +1,61 @@
 // **********************fitre des projet par categorie***************************************************
 
-const boutons = document.querySelectorAll(".filtre");
-const projets = document.querySelectorAll(".projet");
+// const boutons = document.querySelectorAll(".filtre");
+// const projets = document.querySelectorAll(".projet");
 
-boutons.forEach(bouton => {
-    bouton.addEventListener("click", () => {
+// boutons.forEach(bouton => {
+//     bouton.addEventListener("click", () => {
 
-        document.querySelector(".actif").classList.remove("actif");
-        bouton.classList.add("actif");
+//         document.querySelector(".actif").classList.remove("actif");
+//         bouton.classList.add("actif");
 
-        const filtre = bouton.dataset.filter;
+//         const filtre = bouton.dataset.filter;
 
-        projets.forEach(projet => {
+//         projets.forEach(projet => {
 
-            if (filtre === "all" || projet.dataset.category === filtre) {
-                projet.style.display = "block";
-            } else {
-                projet.style.display = "none";
-            }
+//             if (filtre === "all" || projet.dataset.category === filtre) {
+//                 projet.style.display = "block";
+//             } else {
+//                 projet.style.display = "none";
+//             }
 
-        });
+//         });
 
+//     });
+// });
+
+
+// const hamburger = document.querySelector(".menu_hamburger");
+// const menu = document.querySelector("nav");
+
+// hamburger.addEventListener("click", () => {
+//     menu.classList.toggle("active");
+// });
+
+
+// ***************telecharger un cv*********************************************
+const btnCV = document.getElementById("telechargeCv");
+
+if (btnCV) {
+    btnCV.addEventListener("click", function () {
+        const lien = document.createElement("a");
+
+        lien.href = "assets/images/CV_zoundete_k_gisele.pdf";
+        lien.download = "CV-Gisele-Zoundete.pdf";
+
+        document.body.appendChild(lien);
+        lien.click();
+        document.body.removeChild(lien);
     });
-});
+}
 
+// ************************************Navigation avec le menu hamberger***************************
 
-const hamburger = document.querySelector(".menu_hamburger");
-const menu = document.querySelector("nav");
+const menuHamburger = document.getElementById("menuHamburger");
+const menuMobile = document.getElementById("menu");
 
-hamburger.addEventListener("click", () => {
-    menu.classList.toggle("active");
-});
+if (menuHamburger && menuMobile) {
+    menuHamburger.addEventListener("click", () => {
+        menuMobile.classList.toggle("active");
+    });
+}
