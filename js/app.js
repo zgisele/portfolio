8387e32e919e793dd895a962ba1f5301d48d37dc
zgisele <kafouigisele@gmail.com>
2026-07-08 10:40:56 +0000

    ajout du fichier readme

--- js/app.js
+++ js/app.js
@@ -1,37 +1,5 @@
 // **********************fitre des projet par categorie***************************************************
 
-// const boutons = document.querySelectorAll(".filtre");
-// const projets = document.querySelectorAll(".projet");
-
-// boutons.forEach(bouton => {
-//     bouton.addEventListener("click", () => {
-
-//         document.querySelector(".actif").classList.remove("actif");
-//         bouton.classList.add("actif");
-
-//         const filtre = bouton.dataset.filter;
-
-//         projets.forEach(projet => {
-
-//             if (filtre === "all" || projet.dataset.category === filtre) {
-//                 projet.style.display = "block";
-//             } else {
-//                 projet.style.display = "none";
-//             }
-
-//         });
-
-//     });
-// });
-
-
-// const hamburger = document.querySelector(".menu_hamburger");
-// const menu = document.querySelector("nav");
-
-// hamburger.addEventListener("click", () => {
-//     menu.classList.toggle("active");
-// });
-
 
 // ***************telecharger un cv*********************************************
 const btnCV = document.getElementById("telechargeCv");
